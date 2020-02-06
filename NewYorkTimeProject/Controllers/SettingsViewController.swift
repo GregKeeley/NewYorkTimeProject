9fbb504e0d88fee573f1d39e61fee516fd286302
Casandra <casandragrullon@pursuit.org>
2020-02-06 09:47:16 -0500

    set up settings view controller

--- NewYorkTimeProject/Controllers/SettingsViewController.swift
+++ NewYorkTimeProject/Controllers/SettingsViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    private var settingsView = SettingsView()
+    
+    override func loadView() {
+        view = settingsView
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .white
+        settingsView.picker.delegate = self
+        settingsView.picker.dataSource = self
     }
     
+    //TODO: add API data for categories
 
+}
 
+extension SettingsViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        //TODO: fix hard coded number
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        //TODO: fix hard coded number --> add data.count
+        return 1
+    }
+    
+    
 }
