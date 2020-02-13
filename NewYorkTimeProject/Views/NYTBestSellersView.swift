f89f2ad21e7d64843c24d282c01b714f40395885
Juan C <juanceballos@pursuit.org>
2020-02-13 17:44:34 -0500

    UI for bestsellerVC, View, and cell

--- NewYorkTimeProject/Views/NYTBestSellersView.swift
+++ NewYorkTimeProject/Views/NYTBestSellersView.swift
@@ -10,6 +10,60 @@ import UIKit
 
 class NYTBestSellersView: UIView {
 
- 
-
+    public lazy var bestSellerCollectionView: UICollectionView =    {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.itemSize = CGSize(width: 200, height: 200)
+        let collectionView = UICollectionView(frame: CGRect.zero, collectionViewLayout: layout)
+        collectionView.register(BookCell.self, forCellWithReuseIdentifier: "bookCell")
+        collectionView.backgroundColor = .systemTeal
+        return collectionView
+    }()
+    
+    public lazy var genrePickerView: UIPickerView = {
+        let pickerView = UIPickerView()
+        return pickerView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit()   {
+        setupCollectionViewConstraints()
+        setupPickerViewConstraints()
+    }
+    
+    private func setupCollectionViewConstraints() {
+        
+        addSubview(bestSellerCollectionView)
+        bestSellerCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            bestSellerCollectionView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 8),
+            bestSellerCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            bestSellerCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            bestSellerCollectionView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.35)
+        ])
+    }
+    
+    private func setupPickerViewConstraints() {
+        addSubview(genrePickerView)
+        genrePickerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            genrePickerView.topAnchor.constraint(equalTo: bestSellerCollectionView.bottomAnchor, constant: 8),
+            genrePickerView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            genrePickerView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            genrePickerView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -20)
+        ])
+    }
+    
+    
 }
